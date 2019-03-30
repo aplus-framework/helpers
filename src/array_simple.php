ac50b8e600159256747e393ba0a4fec18a5d5376
Natan Felles <natanfelles@gmail.com>
2019-03-30 18:51:02 -0300

    Reformat with new Coding Standards

--- src/array_simple.php
+++ src/array_simple.php
@@ -1,208 +1,155 @@
 <?php
 
+/*
+ * Copyright (C) 2018-2019 Natan Felles <natanfelles@gmail.com>
+ *
+ * Licensed under the MIT license.
+ */
+
 /**
  * Get multidimensional array keys as simple keys.
  *
- * @param array  $array      The array to get the simple keys.
+ * @param array  $array      The array to get the simple keys
  * @param string $simple_key A simple key child. Used by the function itself to mount the keys
  *                           recursively.
  *
- * @return array An array containing the simple keys as values.
+ * @return array An array containing the simple keys as values
  */
-function array_simple_keys(array $array, string $simple_key = ''): array
+function array_simple_keys(array $array, string $simple_key = '') : array
 {
 	static $function_get_child;
-
-	if ($function_get_child === null)
-	{
-		$function_get_child = function (string $key): string {
-			$pos_open  = \strpos($key, '[');
-			$pos_close = $pos_open ? \strpos($key, ']') : false;
-
-			if ($pos_open === false || $pos_close === false || $pos_open > $pos_close)
-			{
+	if ($function_get_child === null) {
+		$function_get_child = function (string $key) : string {
+			$pos_open = strpos($key, '[');
+			$pos_close = $pos_open ? strpos($key, ']') : false;
+			if ($pos_open === false || $pos_close === false || $pos_open > $pos_close) {
 				return '[' . $key . ']';
 			}
-
-			if ($pos_open !== 0)
-			{
-				$key = \explode('[', $key, 2);
+			if ($pos_open !== 0) {
+				$key = explode('[', $key, 2);
 				$key = '[' . $key[0] . '][' . $key[1];
 			}
-
 			return $key;
 		};
 	}
-
 	$all_keys = [];
-
-	foreach ($array as $key => $value)
-	{
-		if (\is_array($value))
-		{
-			if ($simple_key === '')
-			{
-				$all_keys = \array_merge(
+	foreach ($array as $key => $value) {
+		if (is_array($value)) {
+			if ($simple_key === '') {
+				$all_keys = array_merge(
 					$all_keys,
-					\array_simple_keys($value, $key)
+					array_simple_keys($value, $key)
+				);
+			} else {
+				$all_keys = array_merge(
+					$all_keys,
+					array_simple_keys($value, $simple_key . $function_get_child($key))
 				);
 			}
-			else
-			{
-				$all_keys = \array_merge(
-					$all_keys,
-					\array_simple_keys($value, $simple_key . $function_get_child($key))
-				);
-			}
-		}
-		else
-		{
-			if ($simple_key === '')
-			{
+		} else {
+			if ($simple_key === '') {
 				$all_keys[] = $key;
-			}
-			else
-			{
+			} else {
 				$all_keys[] = $simple_key . $function_get_child($key);
 			}
 		}
 	}
-
 	return $all_keys;
 }
 
 /**
  * Get values by a simple key.
  *
- * @param string $simple_key A string in the simple key format.
- * @param array  $array      The array to search in.
+ * @param string $simple_key A string in the simple key format
+ * @param array  $array      The array to search in
  *
- * @return mixed|null The value of the simple key or null if not found.
+ * @return mixed|null The value of the simple key or null if not found
  */
 function array_simple_value(string $simple_key, array $array)
 {
-	$array = \array_simple_revert($array);
-
-	$pos = \strpos($simple_key, '[');
-
-	if ($pos && $pos < \strpos($simple_key, ']'))
-	{
-		\preg_match_all('#\[(.*?)\]#', $simple_key, $matches);
-		$simple_key = \substr($simple_key, 0, $pos);
-		$value      = $array[$simple_key] ?? null;
-
-		foreach ($matches[1] as $match)
-		{
-			if (\is_array($value) && \array_key_exists($match, $value))
-			{
+	$array = array_simple_revert($array);
+	$pos = strpos($simple_key, '[');
+	if ($pos && $pos < strpos($simple_key, ']')) {
+		preg_match_all('#\[(.*?)\]#', $simple_key, $matches);
+		$simple_key = substr($simple_key, 0, $pos);
+		$value = $array[$simple_key] ?? null;
+		foreach ($matches[1] as $match) {
+			if (is_array($value) && array_key_exists($match, $value)) {
 				$value = $value[$match];
-			}
-			else
-			{
+			} else {
 				return null;
 			}
 		}
-
 		return $value;
 	}
-
 	return $array[$simple_key] ?? null;
 }
 
 /**
  * Get an array with simple keys.
  *
- * @param array $array A multidimensional array to be converted into associative simple keys array.
+ * @param array $array A multidimensional array to be converted into associative simple keys array
  *
- * @return array An associative array with the simple keys as keys and their corresponding values.
+ * @return array An associative array with the simple keys as keys and their corresponding values
  */
-function array_simple(array $array): array
+function array_simple(array $array) : array
 {
-	$array = \array_simple_revert($array);
-
+	$array = array_simple_revert($array);
 	$data = [];
-
-	foreach (\array_simple_keys($array) as $key)
-	{
-		$data[$key] = \array_simple_value($key, $array);
+	foreach (array_simple_keys($array) as $key) {
+		$data[$key] = array_simple_value($key, $array);
 	}
-
 	return $data;
 }
 
 /**
  * Converts an array with simple keys format into a PHP multidimensional array.
  *
- * @param array $array_simple An array with simple keys.
+ * @param array $array_simple An array with simple keys
  *
- * @return array An array with native PHP array keys and their corresponding values.
+ * @return array An array with native PHP array keys and their corresponding values
  */
-function array_simple_revert(array $array_simple): array
+function array_simple_revert(array $array_simple) : array
 {
 	static $function_add_child;
-
-	if ($function_add_child === null)
-	{
+	if ($function_add_child === null) {
 		$function_add_child = function (
 			array &$array,
 			array $items,
 			$value,
 			callable $self
-		): void {
-			$key = \array_shift($items);
-
+		) : void {
+			$key = array_shift($items);
 			$array[$key] = [];
-
-			if ($items === [])
-			{
+			if ($items === []) {
 				$array[$key] = $value;
-
 				return;
 			}
-
 			$self($array[$key], $items, $value, $self);
 		};
 	}
-
 	$array = [];
-
-	foreach ($array_simple as $simple_key => $value)
-	{
-		$pos_open  = \strpos($simple_key, '[');
-		$pos_close = $pos_open ? \strpos($simple_key, ']') : false;
-
-		if ($pos_open === false || $pos_close === false || $pos_open > $pos_close)
-		{
-			if (\is_array($array_simple[$simple_key]))
-			{
-				$value = \array_simple_revert($array_simple[$simple_key]);
-			}
-			else
-			{
+	foreach ($array_simple as $simple_key => $value) {
+		$pos_open = strpos($simple_key, '[');
+		$pos_close = $pos_open ? strpos($simple_key, ']') : false;
+		if ($pos_open === false || $pos_close === false || $pos_open > $pos_close) {
+			if (is_array($array_simple[$simple_key])) {
+				$value = array_simple_revert($array_simple[$simple_key]);
+			} else {
 				$value = $array_simple[$simple_key];
 			}
-
 			$array[$simple_key] = $value;
-		}
-		elseif ($pos_open && $pos_open < $pos_close)
-		{
-			\preg_match_all('#\[(.*?)\]#', $simple_key, $matches);
-			$parent_key = \substr($simple_key, 0, $pos_open);
-
+		} elseif ($pos_open && $pos_open < $pos_close) {
+			preg_match_all('#\[(.*?)\]#', $simple_key, $matches);
+			$parent_key = substr($simple_key, 0, $pos_open);
 			$items = [$parent_key];
-
-			foreach ($matches[1] as $match)
-			{
+			foreach ($matches[1] as $match) {
 				$items[] = $match === '' ? 0 : $match;
 			}
-
 			$tree = [];
-
 			$function_add_child($tree, $items, $value, $function_add_child);
-
-			$array = \array_replace_recursive($array, $tree);
+			$array = array_replace_recursive($array, $tree);
 		}
 	}
-
 	return $array;
 }
--- tests/ArraySimpleTest.php
+++ tests/ArraySimpleTest.php
@@ -1,6 +1,14 @@
 <?php
 
-class ArraySimpleTest extends \PHPUnit\Framework\TestCase
+/*
+ * Copyright (C) 2018-2019 Natan Felles <natanfelles@gmail.com>
+ *
+ * Licensed under the MIT license.
+ */
+
+use PHPUnit\Framework\TestCase;
+
+class ArraySimpleTest extends TestCase
 {
 	/**
 	 * @var array
@@ -19,8 +27,8 @@ class ArraySimpleTest extends \PHPUnit\Framework\TestCase
 					'a' => 'aba',
 					'b' => 'abb',
 					'c' => [
-						'a'    => 'abca',
-						'b'    => 'abcb',
+						'a' => 'abca',
+						'b' => 'abcb',
 						'c[a]' => 'abcca',
 					],
 				],
@@ -58,8 +66,7 @@ class ArraySimpleTest extends \PHPUnit\Framework\TestCase
 			'b[b][b]',
 			'b[b][c][a]',
 			'b[b][c][b]',
-		], \array_simple_keys($this->array));
-
+		], array_simple_keys($this->array));
 		$this->assertEquals([
 			'a[a]',
 			'a[b]',
@@ -68,17 +75,15 @@ class ArraySimpleTest extends \PHPUnit\Framework\TestCase
 			'b[c][a]',
 			'b[c][b]',
 			'b[c][c][a]',
-		], \array_simple_keys($this->array['a']));
-
+		], array_simple_keys($this->array['a']));
 		$this->assertEquals([
 			'a',
 			'b',
 			'c[a]',
 			'c[b]',
 			'c[c][a]',
-		], \array_simple_keys($this->array['a']['b']));
-
-		$this->assertEquals([], \array_simple_keys([]));
+		], array_simple_keys($this->array['a']['b']));
+		$this->assertEquals([], array_simple_keys([]));
 	}
 
 	public function testArraySimpleValue()
@@ -99,8 +104,7 @@ class ArraySimpleTest extends \PHPUnit\Framework\TestCase
 					],
 				],
 			],
-		], \array_simple_value('a', $this->array));
-
+		], array_simple_value('a', $this->array));
 		$this->assertEquals([
 			'a' => 'aba',
 			'b' => 'abb',
@@ -111,64 +115,55 @@ class ArraySimpleTest extends \PHPUnit\Framework\TestCase
 					'a' => 'abcca',
 				],
 			],
-		], \array_simple_value('a[b]', $this->array));
-
+		], array_simple_value('a[b]', $this->array));
 		$this->assertEquals([
 			'a' => 'abca',
 			'b' => 'abcb',
 			'c' => [
 				'a' => 'abcca',
 			],
-		], \array_simple_value('a[b][c]', $this->array));
-
-		$this->assertEquals('abca', \array_simple_value('a[b][c][a]', $this->array));
-		$this->assertEquals('abcca', \array_simple_value('a[b][c][c][a]', $this->array));
-
-		$this->assertEquals('bbcb', \array_simple_value('b[b][c][b]', $this->array));
-
-		$this->assertEquals(null, \array_simple_value('a[x]', $this->array));
-
-		$this->assertEquals(null, \array_simple_value('c', $this->array));
-
-		$this->assertEquals(null, \array_simple_value('z', []));
+		], array_simple_value('a[b][c]', $this->array));
+		$this->assertEquals('abca', array_simple_value('a[b][c][a]', $this->array));
+		$this->assertEquals('abcca', array_simple_value('a[b][c][c][a]', $this->array));
+		$this->assertEquals('bbcb', array_simple_value('b[b][c][b]', $this->array));
+		$this->assertNull(array_simple_value('a[x]', $this->array));
+		$this->assertNull(array_simple_value('c', $this->array));
+		$this->assertNull(array_simple_value('z', []));
 	}
 
 	public function testArraySimple()
 	{
 		$this->assertEquals([
-			'a[a][a]'       => 'aaa',
-			'a[a][b]'       => 'aab',
-			'a[b][a]'       => 'aba',
-			'a[b][b]'       => 'abb',
-			'a[b][c][a]'    => 'abca',
-			'a[b][c][b]'    => 'abcb',
+			'a[a][a]' => 'aaa',
+			'a[a][b]' => 'aab',
+			'a[b][a]' => 'aba',
+			'a[b][b]' => 'abb',
+			'a[b][c][a]' => 'abca',
+			'a[b][c][b]' => 'abcb',
 			'a[b][c][c][a]' => 'abcca',
-			'b[a][a]'       => 'baa',
-			'b[a][b]'       => 'bab',
-			'b[b][a]'       => 'bba',
-			'b[b][b]'       => 'bbb',
-			'b[b][c][a]'    => 'bbca',
-			'b[b][c][b]'    => 'bbcb',
-		], \array_simple($this->array));
-
+			'b[a][a]' => 'baa',
+			'b[a][b]' => 'bab',
+			'b[b][a]' => 'bba',
+			'b[b][b]' => 'bbb',
+			'b[b][c][a]' => 'bbca',
+			'b[b][c][b]' => 'bbcb',
+		], array_simple($this->array));
 		$this->assertEquals([
-			'a[a]'       => 'aaa',
-			'a[b]'       => 'aab',
-			'b[a]'       => 'aba',
-			'b[b]'       => 'abb',
-			'b[c][a]'    => 'abca',
-			'b[c][b]'    => 'abcb',
+			'a[a]' => 'aaa',
+			'a[b]' => 'aab',
+			'b[a]' => 'aba',
+			'b[b]' => 'abb',
+			'b[c][a]' => 'abca',
+			'b[c][b]' => 'abcb',
 			'b[c][c][a]' => 'abcca',
-		], \array_simple($this->array['a']));
-
+		], array_simple($this->array['a']));
 		$this->assertEquals([
-			'a'    => 'bba',
-			'b'    => 'bbb',
+			'a' => 'bba',
+			'b' => 'bbb',
 			'c[a]' => 'bbca',
 			'c[b]' => 'bbcb',
-		], \array_simple($this->array['b']['b']));
-
-		$this->assertEquals([], \array_simple([]));
+		], array_simple($this->array['b']['b']));
+		$this->assertEquals([], array_simple([]));
 	}
 
 	public function testArraySimpleRevert()
@@ -176,52 +171,49 @@ class ArraySimpleTest extends \PHPUnit\Framework\TestCase
 		$array = [
 			'[a]' => '[a]',
 			']a[' => ']a[',
-			'a'   => 'a',
-			'b'   => 'b',
-			'c'   => [
+			'a' => 'a',
+			'b' => 'b',
+			'c' => [
 				'a' => 'c[a]',
 			],
-			'd'   => [
+			'd' => [
 				0 => [
 					0 => 'd[0][x]',
 					1 => 'd[0][1]',
 				],
-
 			],
-			'dd'  => [
+			'dd' => [
 				1 => [
 					2 => [
 						'foo bar' => 'dd[1][2][foo bar]',
 					],
 				],
-
 			],
 			'e[x' => 'e[x',
 			'e]x' => 'e]x',
-			'f'   => [
-				0     => '0',
+			'f' => [
+				0 => '0',
 				'one' => 'f[one]',
 				'two' => [
-					0     => 'f[two][0]',
+					0 => 'f[two][0]',
 					'one' => 'f[two][one]',
-					1     => 'f[two][1]',
+					1 => 'f[two][1]',
 				],
 			],
 		];
-
 		$array_simple = [
-			'[a]'               => '[a]',
-			']a['               => ']a[',
-			'a'                 => 'a',
-			'b'                 => 'b',
-			'c[a]'              => 'c[a]',
-			'd[][]'             => 'd[0][0]',
-			'd[][1]'            => 'd[0][1]',
-			'd[][0]'            => 'd[0][x]',
+			'[a]' => '[a]',
+			']a[' => ']a[',
+			'a' => 'a',
+			'b' => 'b',
+			'c[a]' => 'c[a]',
+			'd[][]' => 'd[0][0]',
+			'd[][1]' => 'd[0][1]',
+			'd[][0]' => 'd[0][x]',
 			'dd[1][2][foo bar]' => 'dd[1][2][foo bar]',
-			'e[x'               => 'e[x',
-			'e]x'               => 'e]x',
-			'f'                 => [
+			'e[x' => 'e[x',
+			'e]x' => 'e]x',
+			'f' => [
 				'0',
 				'one' => 'f[one]',
 				'two' => [
@@ -231,7 +223,6 @@ class ArraySimpleTest extends \PHPUnit\Framework\TestCase
 				],
 			],
 		];
-
-		$this->assertEquals($array, \array_simple_revert($array_simple));
+		$this->assertEquals($array, array_simple_revert($array_simple));
 	}
 }
