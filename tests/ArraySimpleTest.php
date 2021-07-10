02f71cb957aadcdd10ada4236beaae448865b500
Natan Felles <natanfelles@gmail.com>
2021-07-10 09:12:36 -0300

    Add header comments

--- tests/ArraySimpleTest.php
+++ tests/ArraySimpleTest.php
@@ -1,5 +1,12 @@
 <?php
-
+/*
+ * This file is part of ArraySimple Library.
+ *
+ * (c) Natan Felles <natanfelles@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 use PHPUnit\Framework\TestCase;
 
 class ArraySimpleTest extends TestCase
