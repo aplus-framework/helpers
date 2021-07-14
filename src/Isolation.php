cab89935c7fc7ef17fbba20f32499107b8b84f71
Natan Felles <natanfelles@gmail.com>
2021-07-14 11:04:13 -0300

    Update default header comments

--- src/Isolation.php
+++ src/Isolation.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 /*
- * This file is part of The Framework Helpers Library.
+ * This file is part of Aplus Framework Helpers Library.
  *
  * (c) Natan Felles <natanfelles@gmail.com>
  *
